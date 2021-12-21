3edeabe5017cf7b12d4e2487387a98dd8346059c
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-12-21 16:00:38 +0100

    Animate circles

--- canvas.js
+++ canvas.js
@@ -3,28 +3,70 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 var context = canvas.getContext("2d");
 
-//draw rectangle
-context.fillStyle = "blue";
-context.fillRect(100, 100, 100, 100);
+//animmate circle
 
-//line
-context.beginPath();
-context.moveTo(50, 300);
-context.lineTo(300, 40);
-context.strokeStyle = "red";
-context.stroke();
+//create circle object
+function Circle(x, y, dx, dy, radius, speed, color) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.speed = speed;
+    this.color = color;
+    //make circle
+    this.draw = function(){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.strokeStyle = this.color;
+        context.stroke();
+        context.fillStyle = this.color;
+        context.fill();
+    }
+    this.update = function(){
+         //change directions when...
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+        //make it move by changing x & y values for the next circle
+        this.x += this.dx;
+        this.y += this.dy;
+        //draw new circle
+        this.draw();
+    }
+}
 
-//arc & circle
-for (var i = 0; i < 4; i++){
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
+//create circles
+//store circles in array
+var circleArray = [];
+for ( var i = 0; i < 50; i++){
+    //set variables
+    var radius = 30;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var speed = 1;
+    var dx = (Math.random() - 0.5) * speed;
+    var dy = (Math.random() - 0.5) * speed;
     var R = Math.random() * 255;
     var G = Math.random() * 255;
     var B = Math.random() * 255;
     var color = `rgb(${R}, ${G}, ${B})`;
-    console.log(color);
-    context.beginPath();
-    context.arc(x, y, 30, 0, Math.PI * 2, false);
-    context.strokeStyle = color;
-    context.stroke();
+    //make circle
+    circleArray.push(new Circle(x, y, dx, dy, radius, speed, color));
 }
+// draw and animate circles
+function animate(){
+    //make a loop
+    requestAnimationFrame(animate);
+    //clear previous input
+    context.clearRect(0, 0, innerWidth, innerHeight);
+    //draw circles
+    for (var i = 0; i < circleArray.length; i++){
+        circleArray[i].update();
+    }
+}
+//call function
+animate();
